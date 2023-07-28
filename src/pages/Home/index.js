d0172519fe84f153f3244f7fd731deebe399060c
Savio-Anjos <savioanjos2020@gmail.com>
2023-07-27 22:43:26 -0300

    :sparkles: feat: Criando header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,90 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+
+import { Feather } from "@expo/vector-icons";
 
 export default function Home() {
+  const [products, setProtucts] = useState([
+    {
+      id: "1",
+      name: "Coca cola",
+      price: 19.9,
+    },
+    {
+      id: "2",
+      name: "Chocolate",
+      price: 6.5,
+    },
+    {
+      id: "4",
+      name: "Queijo 500g",
+      price: 15,
+    },
+    {
+      id: "5",
+      name: "Batata frita",
+      price: 23.9,
+    },
+    {
+      id: "6",
+      name: "Guarana lata",
+      price: 6.0,
+    },
+  ]);
   return (
-    <View style={styles.container}>
-      <Text>Pagina Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.cartContent}>
+        <Text style={styles.title}>Lista de produtos</Text>
+
+        <TouchableOpacity style={styles.cartButton}>
+          <View style={styles.dot}>
+            <Text style={styles.dotText}>3</Text>
+          </View>
+          <Feather name="shopping-cart" size={30} color="#000" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#FAFAFA",
+    paddingEnd: 14,
+    paddingStart: 14,
+  },
+  cartContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 24,
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  dot: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "red",
+    width: 20,
+    height: 20,
+    borderRadius: 12,
+    position: "absolute",
+    zIndex: 99,
+    bottom: -2,
+    left: -4,
+  },
+  dotText: {
+    fontSize: 12,
   },
 });
